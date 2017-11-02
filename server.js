92a40fd8af11381d37db03d6eeeff08adc2c5509
Ammar Mirza <amirza28@uwo.ca>
2017-11-02 22:05:23 +0000

    Can find bears by id

--- server.js
+++ server.js
@@ -58,6 +58,21 @@ router.route('/bears').post(function(req, res)
             res.json(bears);
         });
     });
+    
+    router.route('/bears/:bear_id').get(function(req, res) 
+
+    // get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
+    
+    {
+        Bear.findById(req.params.bear_id, function(err, bear) 
+        {
+            if (err)
+                {
+                    res.send(err);
+                }
+            res.json(bear);
+        });
+    });
 
 
 // REGISTER OUR ROUTES -------------------------------

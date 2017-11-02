27897b9ea211bb3e750b1c2fa51b4632a8742dd7
Ammar Mirza <amirza28@uwo.ca>
2017-11-02 22:17:17 +0000

    Bears can be updated via their id using PUT

--- server.js
+++ server.js
@@ -60,9 +60,7 @@ router.route('/bears').post(function(req, res)
     });
     
     router.route('/bears/:bear_id').get(function(req, res) 
-
     // get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
-    
     {
         Bear.findById(req.params.bear_id, function(err, bear) 
         {
@@ -71,6 +69,28 @@ router.route('/bears').post(function(req, res)
                     res.send(err);
                 }
             res.json(bear);
+        });
+    })
+     .put(function(req, res) 
+     {
+        // use our bear model to find the bear we want
+        Bear.findById(req.params.bear_id, function(err, bear) 
+        {
+            if (err)
+            {
+                res.send(err);
+            }
+            bear.name = req.body.name;  // update the bears info
+            // save the bear
+            bear.save(function(err) 
+            {
+                if (err)
+                {
+                    res.send(err);
+                }
+                res.json({ message: 'Bear updated!' });
+            });
+
         });
     });
 

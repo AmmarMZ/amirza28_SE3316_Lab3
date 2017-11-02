8341f3867b83dcb4704416cacff48eade8183a1a
Ammar Mirza <amirza28@uwo.ca>
2017-11-02 21:35:55 +0000

    Added post functionality

--- server.js
+++ server.js
@@ -31,6 +31,23 @@ router.get('/', function(req, res) {
 
 // more routes for our API will happen here
 
+router.route('/bears').post(function(req, res) 
+{
+    var bear = new Bear();      // create a new instance of the Bear model
+    bear.name = req.body.name;  // set the bears name (comes from the request // save the bear and check for errors
+    bear.save(function(err) 
+    {
+        if (err)
+        {
+            res.send(err);
+            
+        }
+            res.json({ message: 'Bear created!' });
+        });
+
+    });
+
+
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', router);

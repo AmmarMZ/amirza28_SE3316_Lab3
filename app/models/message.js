21d7bc6cbb5ebbe5085c7c9868580c8c3669eb3d
AmmarMZ <ammarmz@hotmail.com>
2017-11-02 22:04:39 -0400

    Delete message.js

--- app/models/message.js
+++
@@ -1,8 +0,0 @@
-var mongoose     = require('mongoose');
-var Schema       = mongoose.Schema;
-
-var messageSchema   = new Schema({
-    message: String
-});
-
-module.exports = mongoose.model('Message', messageSchema);
